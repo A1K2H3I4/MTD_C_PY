0b6c0e65745b24b01d312349e554522e58f7fbdc
AKHILESH BABU <141895914+A1K2H3I4@users.noreply.github.com>
2025-03-06 11:54:26 +0530

    Add files via upload

---
+++ MTD notes.ps1
@@ -0,0 +1,11 @@
+MTD COMPETETIVE PROGRAMMING TRAINING
+
+DAY 1 - THURSDAY - 06/03/2005
+
+IMPORTANT GIT COMMANDS
+1. git status                   //to know the current status of our repo
+2. git clone <url>              //to download the repo
+           git clone https://github.com./neelmyna/vvce_mar25
+3. git pull origin main         //command to download the updated repo from the erver to local machine
+
+
